83b2867b3b0fd90e785cfddc7ac500cb2779eeec
Cipher <nyakangomicah4@gmail.com>
2025-01-13 22:46:09 +0300

    road issue solved

--- road.js
+++ road.js
@@ -1,29 +1,49 @@
-class Road{
-  constructor(x,width,laneCount=3){
-    this.x=x;
-    this.width=width;
-    this.laneCount=laneCount;
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
 
-    this.left=x-width/2;
-    this.right=x+width/2;
+    this.left = x - width / 2;
+    this.right = x + width / 2;
 
     const infinity = 100000;
-    this.top=-infinity
-    this.bottom=infinity
+    this.top = -infinity;
+    this.bottom = infinity;
   }
 
-  draw(ctx){
-    ctx.lineWidth=5;
-    ctx.strokeStyle="white" //white stroke
+  // Calculate the x-coordinate for a specific lane
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount;
+    return this.left + laneWidth / 2 + laneIndex * laneWidth;
+  }
 
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    // Draw road boundaries
     ctx.beginPath();
-    ctx.moveTo(this.left,this.top);
-    ctx.lineTo(this.left,this.bottom);
+    ctx.moveTo(this.left, this.top);
+    ctx.lineTo(this.left, this.bottom);
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(this.right,this.top);
-    ctx.lineTo(this.right,this.bottom);
+    ctx.moveTo(this.right, this.top);
+    ctx.lineTo(this.right, this.bottom);
     ctx.stroke();
+
+    // Draw lane dividers
+    const laneWidth = this.width / this.laneCount;
+    ctx.lineWidth = 2;
+    ctx.setLineDash([20, 20]); // Dashed line
+    for (let i = 1; i < this.laneCount; i++) {
+      const x = this.left + i * laneWidth;
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
+    ctx.setLineDash([]); // Reset to solid lines
   }
 }
